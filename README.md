e95fa9749348a471de264d856427864c496d9755
mphelan <mphelan@cloudaggro.com>
2024-01-27 22:23:47 -0800

    Removing getting the template from Level.sav Including it directly here now. Adding support for numbered deathpenalty values

--- README.md
+++ README.md
@@ -22,46 +22,40 @@ Linux Users will also need to install uesave
 ## Using the EXE
 
 - Shutdown your server
-- Place your PalWorldSettings.ini into the save directory
-- Download palworld-worldoptions.exe from the releases into the save directory
-- Run palworld-worldoptions.exe
-- Start your server and you're good to go!
+- Drag and drop PalWorldSettings.ini onto the exe or run in the same directory
+- Copy WorldOption.sav to your save directory with LevelMeta.sav and Level.sav
+- Start your server and check your settings
 
 Example:
 
-![tutorial](https://private-user-images.githubusercontent.com/16566577/299892843-33ba18d6-fd52-438f-a1a4-a053169bcb27.gif?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDYyNDk3NzgsIm5iZiI6MTcwNjI0OTQ3OCwicGF0aCI6Ii8xNjU2NjU3Ny8yOTk4OTI4NDMtMzNiYTE4ZDYtZmQ1Mi00MzhmLWExYTQtYTA1MzE2OWJjYjI3LmdpZj9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMjYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTI2VDA2MTExOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTBkNmM2YTk2Yjc4MWE3MDhkNzg1NGY3NzMxMGVkMjZiNjhlZjAwYjM5YjA2YmU1NjkzMDU1NzE2YzFmZDlkN2YmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.fDICOve7AD7oP4J-Ma9ZuSPNQ7Vc-nh5hS89k4_Mq8c)
-
+![tutorial](/assets/example.gif)
 ## Using the Scripts
 The exe should work for most people. If you're running a dedicated linux server you can run the tool via the scripts
 
 Help Dialog
 ```console
-usage: palworld-worldoptions [-h] [--uesave UESAVE] [--save_path SAVE_PATH]
+usage: palworld-worldoptions [-h] [--uesave UESAVE] [--output OUTPUT]
+                             [settings_file]
 
 Creates a worldoptions.sav file for dedicated servers
 
+positional arguments:
+  settings_file    location of the PalWorldSettings.ini
+
 options:
-  -h, --help            show this help message and exit
-  --uesave UESAVE       specify uesave location
-  --save_path SAVE_PATH
-                        location of the saved game data folder
+  -h, --help       show this help message and exit
+  --uesave UESAVE  uesave file location
+  --output OUTPUT  output directory for WorldOption.sav
 ```
 
 Example
 ```console
-legoduded@desktop:~/palworld-worldoptions/src/$ python3 main.py --save_path "/mnt/g/pal/SaveGames/0/DDF6BDC891FD4E5DBD8DE895C258C7A8/" --uesave "/home/legoduded/.cargo/bin/uesave"
-
-Found game files
+legoduded@desktop:~/palworld-worldoptions/src$ python3 main.py /mnt/g/PalServer/Pal/Saved/Config/WindowsServer/PalWorldSettings.ini --uesave ~/.cargo/bin/uesave --output /mnt/g/PalServer/Pal/Saved/SaveGames/0/8FAEE1FC44A4A5032BC92F8BCFD43AE3
+Found settings
 Found uesave
-Converting /mnt/g/pal/SaveGames/0/DDF6BDC891FD4E5DBD8DE895C258C7A8/LevelMeta.sav to JSON (using /home/legoduded/.cargo/bin/uesave)
-
-Loading JSON
-Writing JSON to /mnt/g/pal/SaveGames/0/DDF6BDC891FD4E5DBD8DE895C258C7A8/LevelMeta.sav.json
-Loading JSON from /mnt/g/pal/SaveGames/0/DDF6BDC891FD4E5DBD8DE895C258C7A8/WorldOption.sav.json
 Converting JSON
 
-Converted /mnt/g/pal/SaveGames/0/DDF6BDC891FD4E5DBD8DE895C258C7A8/WorldOption.sav.json to /mnt/g/pal/SaveGames/0/DDF6BDC891FD4E5DBD8DE895C258C7A8/WorldOption.sav
-Cleaning up
+Converted /mnt/g/pal/steamcmd/steamapps/common/PalServer/Pal/Saved/SaveGames/0/8FAEE1FC44A4A5032BC92F8BCFD43AE3/WorldOption.sav.json to /mnt/g/pal/steamcmd/steamapps/common/PalServer/Pal/Saved/SaveGames/0/8FAEE1FC44A4A5032BC92F8BCFD43AE3/WorldOption.sav
 Complete!
 Restart your palworld server to apply the changes
 Press RETURN to close
--- src/main.py
+++ src/main.py
@@ -2,12 +2,9 @@
 
 from typing import Dict
 import argparse
-import json
 import sys
 import os
-from lib.rawdata import encode_group_data, decode_group_data
-from lib.noindent import CustomEncoder
-from lib.palsav import convert_to_save, convert_to_json
+from lib.palsav import convert_to_save
 from lib.palworldsettings import create_palworldsettings
 
 
@@ -21,57 +18,25 @@ def exceptionhook(type, value, traceback, oldhook=sys.excepthook):
 sys.excepthook = exceptionhook
 
 
-def convert_single_to_sav(uesave_path: str, save_path: str) -> None:
-    print(f"Loading JSON from {save_path}")
-    with open(save_path, "rb") as f:
-        data = json.load(f)
-    if "worldSaveData" in data["root"]["properties"]:
-        print(f"Encoding GroupSaveDataMap")
-        encode_group_data(data)
-    print(f"Converting JSON")
-    convert_to_save(uesave_path, save_path, data)
-
-
-def convert_single_to_json(uesave_path: str, save_path: str) -> None:
-    print(f"Converting {save_path} to JSON (using {uesave_path})")
-    json_blob = convert_to_json(uesave_path, save_path)
-    if "worldSaveData" in json_blob["root"]["properties"]:
-        print("Decoding GroupSaveDataMap")
-        decode_group_data(json_blob)
-    output_path = save_path + ".json"
-    print(f"Writing JSON to {output_path}")
-    with open(output_path, "w", encoding="utf8") as f:
-        json.dump(json_blob, f, indent=2, cls=CustomEncoder, ensure_ascii=False)
-
-
-def game_files_exist(path: str) -> bool:
-    return os.path.isfile(os.path.join(path, "LevelMeta.sav")) \
-           and os.path.isfile(os.path.join(path, "PalWorldSettings.ini"))
+def game_files_exist(settings_file: str) -> bool:
+    return os.path.isfile(settings_file)
 
 
 def uesave_exists(path: str) -> bool:
     return os.path.exists(path)
 
 
-def load_levelmeta(path: str) -> Dict:
-    with open(os.path.join(path, "LevelMeta.sav.json"), encoding="utf-8") as f:
-        data = json.load(f)
-    data["root"]["properties"].pop("SaveData")
-    return data
+def save_worldoptions(uesave_path: str, worldoption: Dict, output_path: str) -> None:
+    print(f"Converting JSON")
+    convert_to_save(uesave_path, os.path.join(output_path, "WorldOption.sav.json"), worldoption)
 
 
-def save_worldoptions(path: str, data: Dict) -> None:
-    with open(os.path.join(path, "WorldOption.sav.json"), mode="w", encoding="utf-8") as f:
-        json.dump(data, f, indent=2, cls=CustomEncoder, ensure_ascii=False)
-
-
-def convert_to_worldoptions(uesave_path: str, save_path: str) -> None:
+def convert_to_worldoptions(uesave_path: str, settings_file: str, output_path: str) -> None:
     # Make sure files exist
-    if game_files_exist(save_path):
-        print("Found game files")
+    if game_files_exist(settings_file):
+        print("Found settings")
     else:
-        print(f"Could not find LevelMeta.sav and/or PalWorldSettings.ini in {save_path}")
-        print(f"Make sure both files are in the directory and try running again")
+        print(f"Could not find PalWorldSettings.ini at {settings_file}")
         input("Press RETURN to close")
         sys.exit(1)
     if uesave_exists(uesave_path):
@@ -80,36 +45,35 @@ def convert_to_worldoptions(uesave_path: str, save_path: str) -> None:
         print(f"uesave does not exist at {uesave_path}")
         input("Press RETURN to close")
         sys.exit(1)
-    convert_single_to_json(uesave_path, os.path.join(save_path, "LevelMeta.sav"))
-    levelmetadata = load_levelmeta(save_path)
-    optionworlddata = create_palworldsettings(save_path)
-    levelmetadata["root"]["properties"]["OptionWorldData"] = optionworlddata
-    levelmetadata["root"]["save_game_type"] = "/Script/Pal.PalWorldOptionSaveGame"
-    save_worldoptions(save_path, levelmetadata)
-    convert_single_to_sav(uesave_path, os.path.join(save_path, "WorldOption.sav.json"))
-    print("Cleaning up")
-    os.remove(os.path.join(save_path, "LevelMeta.sav.json"))
-    os.remove(os.path.join(save_path, "WorldOption.sav.json"))
+    config_settings_json = create_palworldsettings(settings_file)
+    save_worldoptions(uesave_path, config_settings_json, output_path)
     print("Complete!")
     print("Restart your palworld server to apply the changes")
     input("Press RETURN to close")
 
 
 def main() -> None:
+    running_dir = os.path.dirname(os.path.realpath(sys.executable if getattr(sys, 'frozen', False) else __file__))
     parser = argparse.ArgumentParser(prog='palworld-worldoptions',
                                      description='Creates a worldoptions.sav file for dedicated servers')
+    parser.add_argument('settings_file',
+                        default=os.path.join(running_dir, "PalWorldSettings.ini"),
+                        help='location of the PalWorldSettings.ini',
+                        nargs='?',
+                        )
     parser.add_argument('--uesave',
                         default=os.path.join(
-                            getattr(sys, '_MEIPASS', os.path.dirname(os.path.realpath(__file__))), "uesave", "uesave.exe"
+                            getattr(sys, '_MEIPASS', os.path.dirname(os.path.realpath(__file__))),
+                            "uesave",
+                            "uesave.exe"
                         ),
-                        help='specify uesave location')
-    parser.add_argument('--save_path',
-                        default=os.path.dirname(
-                            os.path.realpath(sys.executable if getattr(sys, 'frozen', False) else __file__)),
-                        help='location of the saved game data folder'
-                        )
+                        help='uesave file location')
+    parser.add_argument('--output',
+                        default=running_dir,
+                        help='output directory for WorldOption.sav')
+
     args = parser.parse_args()
-    convert_to_worldoptions(args.uesave, args.save_path)
+    convert_to_worldoptions(args.uesave, args.settings_file, args.output)
 
 
 if __name__ == '__main__':
